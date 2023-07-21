bd70dc2cb1333b7d63e26efaa39d48d3329cad9f
Marco Haiat <marco.haiat@gmail.com>
2023-07-21 12:53:46 -0300

    feat: game class

--- game.py
+++ game.py
@@ -1,69 +1,95 @@
-import math
-from commands_matrix import create_commands_matrix
-from utils import binary_digit_sum
-from solve_game import solve_game
+from solve_game import GameSolver
+from board import Board
 
-def print_board(lights):
-    side = math.sqrt(len(lights))
-    for l in lights:
-        print(f' {l} |', end=' ')
-        side -= 1
-        if not side:
-            print(' ')
-            side = math.sqrt(len(lights))
-
-
-def is_game_finished(lights):
-    return not any(lights)
-
-
-def game_matrix_operation(lights, move):
-    side = int(math.sqrt(len(lights)))
-    effects_matrix = create_commands_matrix(side)
-    new_lights = map(lambda x, y: binary_digit_sum(x, y), lights, effects_matrix[move - 1])
-    return list(new_lights)
-
-def apply_move(lights, move):
-    new_lights = game_matrix_operation(lights, move)
-    return new_lights
-
-def automatic_mode(lights, solution):
-    for move in solution:
-        input(f'press any key to iterate with move {move}')
-        lights = apply_move(lights, move)
-        print_board(lights)
-      
-    return lights
-
-def game_init():
-    print('Wellcome to Marcos lights out \n')
-    size = int(input("I'll generate a game for you, choose a size: "))
-
-    lights_array = [1 for _ in range(size**2)]
-    print('\n Here is the starting board:\n')
-    print_board(lights_array)
-
+class Game:
     solution = 0
-    while not is_game_finished(lights_array):
-        if not solution:
-          if input('want the solution? (y/n):') == 'y':
-            solution = solve_game(lights_array)
-            print(f'The solution is: {solution}')
-                
-        else:
-            solution = solve_game(lights_array)
-            print(f'The solution is: {solution}')
-            if input('automatic mode? (y/n):') == 'y':
-                lights_array = automatic_mode(lights_array, solution)
-                break
+    finished = False
+    automatic_mode = False
 
-        move = input(f'\ndigite um movimento, de 1 a {len(lights_array)}: ')
-        lights_array = apply_move(lights_array, int(move))
-        print_board(lights_array)
+    def __init__(self, size) -> None:
+        self.size = size
+        self.board = Board(size)
+
+    def start(self):
+        self.greet()
+        print(self.board)
+
+        while not self.finished:
+            if self.solution:
+                if input('Activate automatic Mode?') == 'y':
+                    self.activate_automatic_mode()
+                    break
+            self.game_round()
+            self.finished = self.is_game_finished()
+        
+        self.finish()
+        
+    def game_round(self):
+        if self.solution:
+            self.get_and_show_solution()
+        else:
+            self.ask_about_solution()    
+        move = self.get_next_move()
+        self.board.apply_move(move)
+        print(self.board)
+
+    def activate_automatic_mode(self):
+        for move in self.solution:
+            input(f'press any key to iterate with move {move}')
+            self.board.apply_move(move)
+            print(self.board)
+    
+    def ask_about_solution(self):
+        if input('Want to see the solution? (y):') == 'y':
+            self.get_and_show_solution()
+    
+    def get_and_show_solution(self):
+        solver = GameSolver(self.size)
+        self.solution = solver.get_solution(self.board.lights)
+        print(self.solution)
+
+    def is_game_finished(self):
+        return not any(self.board.lights)
+
+    def get_next_move(self) -> int:
+        next_move = input(f'input a number from 1 to {len(self.board)}: ')
+        return int(next_move)        
+
+    def greet(self):
+        print(f'game initialized with a board of size {self.size}')
+
+    def finish(self):
+        print('Congratulations!!! Geniouss')
+
+# def game_init():
+    # print('Wellcome to Marcos lights out \n')
+    # size = int(input("I'll generate a game for you, choose a size: "))
+
+    # lights_array = [1 for _ in range(size**2)]
+    # print('\n Here is the starting board:\n')
+    # print_board(lights_array)
+
+    # solution = 0
+    # while not is_game_finished(lights_array):
+    #     if not solution:
+    #       if input('want the solution? (y/n):') == 'y':
+    #         solution = solve_game(lights_array)
+    #         print(f'The solution is: {solution}')
+                
+    #     else:
+    #         solution = solve_game(lights_array)
+    #         print(f'The solution is: {solution}')
+    #         if input('automatic mode? (y/n):') == 'y':
+    #             lights_array = automatic_mode(lights_array, solution)
+    #             break
+
+    #     move = input(f'\ndigite um movimento, de 1 a {len(lights_array)}: ')
+    #     lights_array = apply_move(lights_array, int(move))
+    #     print_board(lights_array)
             
     
-    print('Congratulations!!! Geniouss')
-    return
+    # print('Congratulations!!! Geniouss')
+    # return
 
 
-game_init()
+# game_init()
